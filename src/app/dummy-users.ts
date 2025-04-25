4179b1708561d89bbf9ae5e39e39efc263e37f25
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-04-25 10:29:00 +0200

    Vid 19

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+    {
+      id: 'u1',
+      name: 'Jasmine Washington',
+      avatar: 'stock1.jpg',
+    },
+    {
+      id: 'u2',
+      name: 'Emily Thompson',
+      avatar: 'stock2.jpg',
+    },
+    {
+      id: 'u3',
+      name: 'Marcus Johnson',
+      avatar: 'stock3.jpg',
+    },
+    {
+      id: 'u4',
+      name: 'David Miller',
+      avatar: 'stock4.jpg',
+    },
+    {
+      id: 'u5',
+      name: 'Priya Patel',
+      avatar: 'stock5.jpg',
+    },
+    {
+      id: 'u6',
+      name: 'Arjun Singh',
+      avatar: 'stock6.jpg',
+    },
+  ];

f362b148bc0c5561cc7ece75ab37e2e1b5fffdca
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-04-25 13:36:05 +0200

    Vid 34

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
-
 
 @Component({
   selector: 'app-user',
@@ -10,13 +9,16 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
   get imgPath() {
     return 'assets/users/' + this.avatar;
   }
 
   onSelectUser() {
+    this.select.emit(this.id);
   }
 }

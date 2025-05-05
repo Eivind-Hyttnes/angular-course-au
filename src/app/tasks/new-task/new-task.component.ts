5b499a7d5c9891de7b479cdecab30fbfb4304c71
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-05-05 11:56:59 +0200

    vid 54

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { input, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.scss'
+  styleUrl: './new-task.component.scss',
 })
 export class NewTaskComponent {
   cancel = output<void>();
+  add = output<NewTaskData>();
   enteredTitle = '';
   enteredSummary = '';
   enteredDate = '';
 
   onCancel() {
     this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
   }
 }

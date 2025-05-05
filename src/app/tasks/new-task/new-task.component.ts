401a0c6195a23f1431db31372c8cfb19fc158b0c
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-05-05 10:23:31 +0200

    vid 51, glemte å legge til de siste par videoene

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
 import { input, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.scss'
 })
 export class NewTaskComponent {
   cancel = output<void>();
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
 
   onCancel() {
     this.cancel.emit();

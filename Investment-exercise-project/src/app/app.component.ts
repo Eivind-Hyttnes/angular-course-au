1c1e94476a350ad8579535f896f6561dc3e5f02e
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-09 11:20:54 +0200

    vid 80

--- Investment-exercise-project/src/app/app.component.ts
+++ Investment-exercise-project/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { IncestmentResultsComponent } from "./incestment-results/incestment-results.component";
-import { UserInputComponent } from "./user-input/user-input.component";
+import { IncestmentResultsComponent } from './incestment-results/incestment-results.component';
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
   selector: 'app-root',
@@ -10,5 +10,31 @@ import { UserInputComponent } from "./user-input/user-input.component";
   imports: [HeaderComponent, IncestmentResultsComponent, UserInputComponent],
 })
 export class AppComponent {
-  imports = [HeaderComponent];
+  calculateInvestmentResults(data: {
+    initialInvestment: number,
+    duration: number,
+    expectedReturn: number,
+    annualInvestment: number,
+  }) {
+    const annualData = [];
+    let investmentValue = data.initialInvestment;
+
+    for (let i = 0; i < data.duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (data.expectedReturn / 100);
+      investmentValue += interestEarnedInYear + data.annualInvestment;
+      const totalInterest =
+        investmentValue - data.annualInvestment * year - data.initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: data.annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: data.initialInvestment + data.annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
 }

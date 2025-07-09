1a961f3f02aa0001fa617b881d590be410339052
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-09 13:37:40 +0200

    vid 85

--- Investment-exercise-project/src/app/investment-results/investment-results.component.ts
+++ Investment-exercise-project/src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,12 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, input } from '@angular/core';
+
 import type { IinvestmentResult } from '../input.model';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })

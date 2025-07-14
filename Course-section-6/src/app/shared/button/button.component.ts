fb2487146e95abfef026924f92765e9ec7ff25eb
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-14 13:34:41 +0200

    vid 106

--- Course-section-6/src/app/shared/button/button.component.ts
+++ Course-section-6/src/app/shared/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'button[appButton]',
@@ -7,6 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
-export class ButtonComponent {
-
-}
+export class ButtonComponent {}

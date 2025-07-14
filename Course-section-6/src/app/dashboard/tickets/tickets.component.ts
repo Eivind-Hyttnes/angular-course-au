8087744cb169f393e8accadcafe725d3f1b8933c
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-14 12:57:45 +0200

    vid 103

--- Course-section-6/src/app/dashboard/tickets/tickets.component.ts
+++ Course-section-6/src/app/dashboard/tickets/tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NewTickerComponent } from "./new-ticket/new-ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [],
+  imports: [NewTickerComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })

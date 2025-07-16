f1941a627ec7f240c310814526e44f120b06f82d
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-16 14:07:49 +0200

    vid 126

--- Course-section-6/src/app/dashboard/server-status/server-status.component.ts
+++ Course-section-6/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+  private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -23,13 +23,14 @@ export class ServerStatusComponent implements OnInit, OnDestroy {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+      console.log('ServerStatusComponent destroyed, interval cleared');
+    });
   }
 
   ngAfterViewInit() {
     console.log('ServerStatusComponent view initialized');
-  }
-
-  ngOnDestroy() {
-    clearTimeout(this.interval);
   }
 }

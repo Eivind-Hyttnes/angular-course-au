e0024414fea2eeac60d0c7eeb247db3816cd62b9
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-17 14:04:09 +0200

    vid 154

--- Course-section-7/src/app/safe-link.directive.ts
+++ Course-section-7/src/app/safe-link.directive.ts
@@ -1,11 +1,26 @@
-import { Directive } from "@angular/core";
+import { Directive } from '@angular/core';
 
 @Directive({
-    selector: 'a[appSafeLink]',
-    standalone: true,
+  selector: 'a[appSafeLink]',
+  standalone: true,
+  host: {
+    '(click)': 'onConfirmLeavePage($event)',
+  },
 })
 export class SafeLinkDirective {
-    constructor() {
-        console.log('SafeLinkDirective is active!');
+  constructor() {
+    console.log('SafeLinkDirective is active!');
+  }
+
+  onConfirmLeavePage(event: MouseEvent) {
+    const wantsToLeave = window.confirm(
+      'Do you really want to leave this page?'
+    );
+
+    if (wantsToLeave) {
+      return;
     }
-} 
+
+    event.preventDefault();
+  }
+}

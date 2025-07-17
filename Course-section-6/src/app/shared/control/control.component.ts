6fc0ab2b37c7757307465dfa46043f4454d8e982
Eivind-Hyttnes <eivind.hyttnes@autility.io>
2025-07-17 09:04:11 +0200

    vid 135

--- Course-section-6/src/app/shared/control/control.component.ts
+++ Course-section-6/src/app/shared/control/control.component.ts
@@ -1,5 +1,7 @@
 import {
   AfterContentInit,
+  afterNextRender,
+  afterRender,
   Component,
   ContentChild,
   contentChild,
@@ -31,6 +33,16 @@ export class ControlComponent implements AfterContentInit {
 
   private control =
     contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input');
+
+    constructor() {
+      afterRender(() => {
+        console.log('afterRender');
+      });
+
+      afterNextRender(() => {
+        console.log('afterNextRender');
+      });
+    }
 
     ngAfterContentInit() {
       
